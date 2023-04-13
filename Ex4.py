9e7cb48b7b0a12e5bb7465b3e0dc85b3de2aa015
Germain712 <102153309+Germain712@users.noreply.github.com>
2023-04-13 11:08:38 +0100

    Initial commit

---
+++ Ex4.py
@@ -0,0 +1,17 @@
+count = 0
+count += 5
+count = count * 2
+print(count, type(count))
+
+count *= 1.0
+print(count, type(count))
+
+
+title = "Jaws"
+duration = 91.0
+year = 1994
+summary = "My favourite movie is " + title + \
+          ". It was filmed in " + str(year) + \
+          " and runs for " + str(duration) + \
+          " mins"
+print(summary)
